3b7f76cfd5155dcf77a98e490f668be1adf9e957
oswalag <119532580+oswalag@users.noreply.github.com>
2022-11-30 23:48:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ medicionRef.on("value", (snapshot) => {
     console.log("Valor Medido: " + snapshot.val());
     consumo = snapshot.val();
       document.getElementById("calidad").innerHTML =
-        Number.parseFloat(consumo).toFixed(2) + " ug/m";
+        Number.parseFloat(consumo).toFixed(2) + " ug/m³";
         if(consumo < 40){
+          document.getElementById("mensaje").innerHTML = "Buena"; 
           document.querySelector('.circle1').style.backgroundColor = '#09FF06';
           document.querySelector('.circle2').style.backgroundColor = '#09FF06';
         } else if(consumo >= 40 && consumo <= 100){
+          document.getElementById("mensaje").innerHTML = "Regular";
           document.querySelector('.circle1').style.backgroundColor = '#FFFF06';
           document.querySelector('.circle2').style.backgroundColor = '#FFFF06';
         } else if(consumo > 100){
+          document.getElementById("mensaje").innerHTML = "Mala";
           document.querySelector('.circle1').style.backgroundColor = '#FF0606';
           document.querySelector('.circle2').style.backgroundColor = '#FF0606';
         }
@@ -21,4 +24,3 @@ medicionRef.on("value", (snapshot) => {
     .catch((error) => {
       console.log(error);
 });
-
